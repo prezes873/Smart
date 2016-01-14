7f31aba1923420bf79772ea6599540eaa9a1312d
prezes873 <maciekstodulski@o2.pl>
2016-01-14 19:47:50 +0100

    next

--- smartHMATest/app/src/main/java/pl/wasat/smarthma/ui/frags/base/BaseSearchSideParametersFragment.java
+++ smartHMATest/app/src/main/java/pl/wasat/smarthma/ui/frags/base/BaseSearchSideParametersFragment.java
@@ -1,8 +1,12 @@
 package pl.wasat.smarthma.ui.frags.base;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
@@ -10,10 +14,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.SearchView;
 import android.widget.TextView;
 
+import java.util.HashMap;
+
 import pl.wasat.smarthma.R;
+import pl.wasat.smarthma.helper.Const;
+import pl.wasat.smarthma.preferences.GlobalPreferences;
+import pl.wasat.smarthma.preferences.SharedPrefs;
+import pl.wasat.smarthma.ui.activities.SearchCollectionResultsActivity;
+import pl.wasat.smarthma.ui.frags.search.SearchFragment;
+import pl.wasat.smarthma.ui.tooltips.Tooltip;
 
 /**
  * A simple {@link android.support.v4.app.Fragment} subclass. Activities that
@@ -31,14 +46,19 @@ public class BaseSearchSideParametersFragment extends BaseDateTimeAreaContainerF
             "EOP:ESA:FEDEO:COLLECTIONS", "EOP:ESA:GPOD-EO", "EOP:ESA:EO-VIRTUAL-ARCHIVE4",
             "EOP:ESA:SMOS", "EOP:JAXA:CATS-I", "EOP:NASA:ECHO"};
 
+    private SearchFragment.OnSearchFragmentListener mListener;
+    private Intent searchIntent;
+    private Tooltip tooltip;
+    private HashMap extraParams;
+
     /**
      * Use this factory method to create a new instance of this fragment using
      * the provided parameters.
      *
      * @return A new instance of fragment SearchBasicInfoRightFragment.
      */
-    public static BaseSearchSideParametersFragment newInstance(boolean onePanel) {
-        one_panel = onePanel;
+    public static BaseSearchSideParametersFragment newInstance() {
+
         return new BaseSearchSideParametersFragment();
     }
 
@@ -53,11 +73,66 @@ public class BaseSearchSideParametersFragment extends BaseDateTimeAreaContainerF
         {
             rootView = inflater.inflate(
                     R.layout.fragment_search_side_parameters_one_panel, container, false);
+            SearchManager searchManager = (SearchManager) getActivity()
+                    .getSystemService(Context.SEARCH_SERVICE);
+
+            final SearchView searchView = (SearchView) rootView
+                    .findViewById(R.id.search_frag_searchview);
+            searchView.setSearchableInfo(searchManager
+                    .getSearchableInfo(getActivity().getComponentName()));
+
+            searchView.setSubmitButtonEnabled(true);
+            searchView.setFocusable(true);
+            searchView.setIconified(false);
+            searchView.clearFocus();
+
+            GlobalPreferences globalPreferences = new GlobalPreferences(getActivity());
+            if (globalPreferences.getIsParamsSaved()) {
+                SharedPrefs sharedPrefs = new SharedPrefs(getActivity());
+                searchView.setQuery(sharedPrefs.getQueryPrefs(), false);
+            }
+
+            searchIntent = getActivity().getIntent();
+
+            LinearLayout linearLayout1 = (LinearLayout) searchView.getChildAt(0);
+            LinearLayout linearLayout2 = (LinearLayout) linearLayout1.getChildAt(2);
+            LinearLayout linearLayout3 = (LinearLayout) linearLayout2.getChildAt(1);
+            AutoCompleteTextView autoComplete = (AutoCompleteTextView) linearLayout3
+                    .getChildAt(0);
+            autoComplete.setTextSize(20);
+            autoComplete.setTextColor(getResources().getColor(R.color.text_black));
+
+            Button btnGo = (Button) rootView.findViewById(R.id.search_frag_button_start);
+            btnGo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startSearchWithButton(searchView);
+                }
+            });
+
+
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String s) {
+                    mListener.onSearchFragmentSendExtraParams(extraParams);
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String s) {
+                    return false;
+                }
+            });
+
+            tooltip = new Tooltip(getContext(), Tooltip.TYPE_BELOW, "Greetings from below!");
+
+            extraParams = new HashMap();
         }
         else {
             rootView = inflater.inflate(
                     R.layout.fragment_search_side_parameters, container, false);
         }
+
 
         tvCatalogName = (TextView) rootView
                 .findViewById(R.id.search_frag_side_params_tv_catalog_name);
@@ -144,6 +219,27 @@ public class BaseSearchSideParametersFragment extends BaseDateTimeAreaContainerF
                 "CatalogueListDialogFragment");
     }
 
+    private void startSearchWithButton(SearchView searchView) {
+        Intent intentBtnSearch = new Intent(getActivity(),
+                SearchCollectionResultsActivity.class);
+        intentBtnSearch.setAction("android.intent.action.SEARCH");
+        intentBtnSearch.putExtra(SearchManager.QUERY, searchView.getQuery().toString());
+        intentBtnSearch.putExtra(Const.KEY_INTENT_FEDEO_REQUEST_PARAMS_EXTRA, extraParams);
+
+        mListener.onSearchFragmentStartSearchingWithButton(intentBtnSearch);
+    }
+
+    public void setAdditionalParams(String parameterKey, String parameterValue) {
+        extraParams.put(parameterKey, parameterValue);
+    }
+
+    public void setQuery(String query) {
+        if (rootView == null) return;
+        SearchView searchView = (SearchView) rootView.findViewById(R.id.search_frag_searchview);
+        if (searchView != null) searchView.setQuery(query, false);
+    }
+
+
     public static class CatalogueListDialogFragment extends DialogFragment {
         @NonNull
         @Override
@@ -173,9 +269,38 @@ public class BaseSearchSideParametersFragment extends BaseDateTimeAreaContainerF
         }
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Activity activity = context instanceof Activity ? (Activity) context : null;
+        try {
+            mListener = (SearchFragment.OnSearchFragmentListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnSearchFragmentListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
     @SuppressWarnings("SameReturnValue")
     private static CharSequence[] getCataloguesList() {
         return cataloguesList;
+    }
+
+    public interface OnSearchFragmentListener {
+        void onSearchFragmentBasicParamsChoose();
+
+        void onSearchFragmentAdvanceParamsChoose();
+
+        void onSearchFragmentSendExtraParams(HashMap extra);
+
+        void onSearchFragmentStartSearchingWithButton(Intent intent);
+
     }
 
 }
